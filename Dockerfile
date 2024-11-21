47cebe81b8b159a3667e95891e2817440351f1d1
Gregory Diamos <gregorydiamos@MacBook-Air-Gregory.local>
2024-11-21 06:34:19 -0800

    slurmctld

--- Dockerfile
+++ Dockerfile
@@ -104,5 +104,3 @@ COPY ./cray ${INSTALL_ROOT}/cray
 COPY ./ml ${INSTALL_ROOT}/ml
 COPY ./scripts ${INSTALL_ROOT}/scripts
 
-RUN chmod 400 ${INSTALL_ROOT}/infra/slurm_configs/slurm.key
-
--- scripts/start_slurm.sh
+++ scripts/start_slurm.sh
@@ -15,6 +15,8 @@ LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 # Run the slurm discovery service
 python $LOCAL_DIRECTORY/../infra/cray_infra/slurm/discovery/discover_clusters.py
 
-slurmctld &
-slurmd &
+chmod 400 /app/cray/infra/slurm_configs/slurm.key
+
+slurmctld -Dvvv &
+slurmd -Dvvv &
 
