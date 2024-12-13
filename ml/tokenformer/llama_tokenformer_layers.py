199f44f20486b2fbd41cbe1bb995ed7d46dd9004
Naila Farooqui <gtnaila@gmail.com>
2024-12-13 14:06:53 -0800

    Tokenformer adapter implementation.

---
+++ ml/tokenformer/llama_tokenformer_layers.py
@@ -0,0 +1,161 @@
+import torch
+from typing import Optional, Tuple
+import torch.nn as nn
+from transformers.models.llama.modeling_llama import LlamaSdpaAttention, apply_rotary_pos_emb, LlamaDecoderLayer
+from transformers.models.llama.configuration_llama import LlamaConfig 
+from transformers.cache_utils import Cache
+
+def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """
+    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
+    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
+    """
+    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
+    if n_rep == 1:
+        return hidden_states
+    hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
+    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
+
+
+class Attention(nn.Module):
+    def __init__(
+        self) -> None:
+        super().__init__()
+    
+    def forward(self, query, key, value, attn_mask=None, dropout_p=0.0, is_causal=False):
+        
+        attn_output = torch.nn.functional.scaled_dot_product_attention(
+            query=query,
+            key=key, 
+            value=value,
+            attn_mask=attn_mask,
+            dropout_p=dropout_p,
+            is_causal=is_causal,
+        )
+        return attn_output
+
+class LlamaTokenformerAttention(LlamaSdpaAttention):
+    def __init__(self, config, layer_idx: int):
+        super().__init__(config, layer_idx)
+        self.attn = Attention()
+        
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None, 
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+        if position_embeddings is None:
+            cos, sin = self.rotary_emb(value_states, position_ids)
+        else:
+            cos, sin = position_embeddings
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+        if past_key_value is not None:
+            # sin and cos are specific to RoPE models; cache_position needed for the static cache
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        causal_mask = attention_mask
+        if attention_mask is not None:
+            causal_mask = causal_mask[:, :, :, : key_states.shape[-2]]
+
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and causal_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
+
+        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+        is_causal = True if causal_mask is None and q_len > 1 else False
+
+        # Self Attention
+        attn_output = self.attn(
+            query_states,
+            key_states,
+            value_states,
+            causal_mask,
+            self.attention_dropout if self.training else 0.0,
+            is_causal,
+        )
+
+        attn_output = attn_output
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, -1)
+
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
+
+class LlamaTokenformerDecoderLayer(LlamaDecoderLayer):
+    def __init__(self, config: LlamaConfig, layer_idx: int):
+        super().__init__(config, layer_idx)
+        self.self_attn = LlamaTokenformerAttention(config, layer_idx)
+        
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: Optional[bool] = False,
+        use_cache: Optional[bool] = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None, 
+        **kwargs,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        
+        residual = hidden_states
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+        )
+        hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        
+        hidden_states = self.mlp(hidden_states)
+
+        hidden_states = residual + hidden_states
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (attn_weights,)
+
+        if use_cache:
+            outputs += (present_key_value,)
+
+        return outputs
--- ml/tokenformer/llama_tokenformer_model.py
+++ ml/tokenformer/llama_tokenformer_model.py
@@ -1,117 +1,8 @@
-import torch
-from torch import nn
-from transformers import LlamaForCausalLM
-from ml.tokenformer.llama_tokenformer_decoder_layer import LlamaTokenformerDecoderLayer
+from ml.tokenformer.llama_tokenformer_layers import LlamaTokenformerDecoderLayer
+from ml.tokenformer.tokenformer_adapter_manager import TokenformerModelManager
 import logging
 
 logger = logging.getLogger(__name__)
-
-class TokenformerMLPAdapter(nn.Module):
-    def __init__(self, layer, hidden_size):
-        super().__init__()
-        self.layer = layer
-        self.hidden_size = hidden_size
-    
-        self.tokenformer_k = nn.Parameter(torch.randn(hidden_size, hidden_size))
-        self.tokenformer_v = nn.Parameter(torch.zeros(hidden_size, hidden_size))
-
-    # Call layer with all inputs and kwargs
-    def forward(
-        self,
-        query: torch.Tensor
-    ):
-        base_layer_results = self.layer(query)
-        
-        tokenformer_results = torch.nn.functional.scaled_dot_product_attention(
-            query=query, key=self.tokenformer_k, value=self.tokenformer_v,
-            attn_mask=None,
-            is_causal=False # should be false for tokenformer
-        )
-        
-        # sum the two outputs
-        layer_and_adaptor_sum = base_layer_results + tokenformer_results
-        return layer_and_adaptor_sum    
-
-class TokenformerAttentionAdapter(nn.Module):
-    def __init__(self, layer, hidden_size):
-        super().__init__()
-        self.layer = layer
-        self.hidden_size = hidden_size
-    
-        self.tokenformer_k = nn.Parameter(torch.randn(hidden_size, hidden_size))
-        self.tokenformer_v = nn.Parameter(torch.zeros(hidden_size, hidden_size))
-
-    # Call layer with all inputs and kwargs
-    def forward(
-        self,
-        query,
-        key,
-        value,
-        attn_mask,
-        dropout_p,
-        is_causal
-    ):
-        base_layer_results = self.layer(query, key, value, attn_mask, dropout_p, is_causal)
-        
-        tokenformer_results = torch.nn.functional.scaled_dot_product_attention(
-            query=query, 
-            key=self.tokenformer_k, 
-            value=self.tokenformer_v,
-            attn_mask=None,
-            dropout_p=0.0,
-            is_causal=False # should be false for tokenformer
-        )
-        
-        # sum the two outputs
-        layer_and_adaptor_sum = base_layer_results + tokenformer_results
-        return layer_and_adaptor_sum    
-
-def is_attn_layer(layer_name):
-    if layer_name.split('.')[-1] == "attn": 
-        return True
-    return False
-
-def is_mlp_layer(layer_name):
-    if "mlp" in layer_name.split('.')[-1]: 
-        return True
-    return False
-    
-    
-def recursive_setattr(obj, attr, value):
-    attr = attr.split(".", 1)
-    if len(attr) == 1:
-        setattr(obj, attr[0], value)
-    else:
-        recursive_setattr(getattr(obj, attr[0]), attr[1], value)    
-
-
-def try_to_update_mlp(name, layer, model):
-    """Try to wrap the layer with a TokenformerMLPAdaptor."""
-    if not is_mlp_layer(name):
-        return
-
-    logger.info(f"Wrapping layer {name} with TokenformerMLPAdaptor")
-
-    # Wrap the layer with a TokenformerMLPAdapter
-    recursive_setattr(model, name, TokenformerMLPAdapter(layer, model.config.hidden_size))
-
-
-def try_to_update_attn(name, layer, model):
-    """Try to wrap the layer with a TokenformerAttentionAdaptor."""
-    if not is_attn_layer(name):
-        return
-
-    logger.info(f"Wrapping layer {name} with TokenformerAttentionAdaptor")
-
-    # Wrap the layer with a TokenformerAttentionAdapter
-    recursive_setattr(model, name, TokenformerAttentionAdapter(layer, model.config.intermediate_size))
-
-def add_tokenformer_adapters(model):
-    # Add tokenformer adapters for mlp and attention
-    for name, layer in model.named_modules():
-        try_to_update_mlp(name, layer, model)
-        try_to_update_attn(name, layer, model)
-    return model
 
 def replace_layers(model, custom_layer_class):
     # Replace layers with custom layers
@@ -123,17 +14,17 @@ def replace_layers(model, custom_layer_class):
 
 def create_llama_tokenformer_model(model):
     model = replace_layers(model, LlamaTokenformerDecoderLayer)
-    model = add_tokenformer_adapters(model)
+    tokenformer_model_manager = TokenformerModelManager(model)
+    tokenformer_model = tokenformer_model_manager.model
     # Set requires_grad to False for all parameters in the model
-    for param in model.parameters():
+    for param in tokenformer_model.parameters():
         param.requires_grad = False
 
     # Set requires_grad to True for tokenformer params and lm_head
-    for name, param in model.named_parameters():
+    for name, param in tokenformer_model.named_parameters():
         for item in ["tokenformer", "lm_head"]:
             if item in name:
                 param.requires_grad = True
         logger.debug(f"Parameter: {name}, Requires Grad: {param.requires_grad}")
     
-    return model
-
+    return tokenformer_model
