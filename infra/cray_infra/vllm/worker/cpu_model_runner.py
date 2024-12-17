d411ccefe853857a99182b1506722112c614be06
Naila Farooqui <gtnaila@gmail.com>
2024-12-17 11:26:19 -0800

    update

--- infra/cray_infra/vllm/worker/cpu_model_runner.py
+++ infra/cray_infra/vllm/worker/cpu_model_runner.py
@@ -585,8 +585,9 @@ class CPUModelRunner(ModelRunnerBase[ModelInputForCPU]):
         
         if self.lora_config:
             assert model_input.lora_requests is not None
-            for lora_request in model_input.lora_requests and lora_request is not None:
-                self.tokenformer_manager.add_adapter(lora_request)
+            for lora_request in model_input.lora_requests:
+                if lora_request is not None:
+                    self.tokenformer_manager.add_adapter(lora_request)
             model_executable = self.tokenformer_manager._adapter_manager.model
 
         execute_model_kwargs = {
