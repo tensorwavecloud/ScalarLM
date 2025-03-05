929518237a9ccb21f87b8adef18331006e951596
Greg Diamos <gregory.diamos@gmail.com>
2025-03-05 02:01:58 -0800

    Handle calculation of max batch size

--- infra/cray_infra/one_server/create_vllm.py
+++ infra/cray_infra/one_server/create_vllm.py
@@ -25,7 +25,7 @@ async def create_vllm(port, running_status):
     args = parser.parse_args(args=[
         f"--dtype={config['dtype']}",
         f"--max-model-len={config['max_model_length']}",
-        f"--max-num-batched-tokens={config['generate_batch_size'] * config['max_model_length']}",
+        f"--max-num-batched-tokens={config['max_model_length']}",
         f"--max-seq-len-to-capture={config['max_model_length']}",
         f"--gpu-memory-utilization={config['gpu_memory_utilization']}",
         f"--max-log-len={config['max_log_length']}",
