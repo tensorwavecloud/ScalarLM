53082db406ead53ea7cc5d1fa36bb3f386e28d7e
Naila Farooqui <gtnaila@gmail.com>
2025-03-07 00:03:51 -0800

    update tests.

--- test/ml/tokenformer/test_llama_tokenformer_model.py
+++ test/ml/tokenformer/test_llama_tokenformer_model.py
@@ -14,7 +14,8 @@ def model_setup():
 
 def test_create_llama_tokenformer_model(model_setup):
     model = model_setup
-    result = create_llama_tokenformer_model(model)
+    # lm_head is trained by default
+    result = create_llama_tokenformer_model(model, "cpu")
 
     # Check requires_grad is set correctly 
     for name, param in result.named_parameters():
@@ -23,3 +24,14 @@ def test_create_llama_tokenformer_model(model_setup):
         else:
             assert not param.requires_grad
 
+def test_create_llama_tokenformer_model_no_lm_head(model_setup):
+    model = model_setup
+    # lm_head should not be trained
+    result = create_llama_tokenformer_model(model=model, device="cpu", train_lm_head=False)
+
+    # Check requires_grad is set correctly 
+    for name, param in result.named_parameters():
+        if any(module_name in name for module_name in ["tokenformer"]):
+            assert param.requires_grad
+        else:
+            assert not param.requires_grad
