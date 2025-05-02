32abd3a3544588dcdd5d53a056bf095669ec5bd8
Sudnya Diamos <sudnyadiamos@gmail.com>
2025-05-01 20:46:33 -0700

    Latest deployment of v0.7 on amd cluster (#81)
    * Latest deployment of v0.7 on amd cluster

--- test/deployment/generate.py
+++ test/deployment/generate.py
@@ -1,11 +1,6 @@
 import scalarlm
 
-# scalarlm.api_url = "http://localhost:8000"
-# scalarlm.api_url = "https://tensorwave.cray-lm.com"
-scalarlm.api_url = "https://llama8btensorwave.cray-lm.com"
-# scalarlm.api_url = "https://vultr.sscalarlm.com"
-# scalarlm.api_url = "https://meta-llama--llama-3-2-3b-instruct.cray-lm.com"
-
+scalarlm.api_url = "http://localhost:8000"
 
 def get_dataset(count):
     dataset = []
@@ -23,8 +18,6 @@ dataset = get_dataset(count=1)
 results = llm.generate(
     prompts=dataset,
     max_tokens=200,
-    # generate with default model
-    # model_name="2560805ddf87f7a1340be14e4b3dd38f7289a43de79410b3ac09944438842bfd"
 )
 
 print(results)
--- test/deployment/train.py
+++ test/deployment/train.py
@@ -1,13 +1,6 @@
 import scalarlm
 
-# scalarlm.api_url = "http://localhost:8000"
-# scalarlm.api_url = "https://tensorwave.cray-lm.com"
-scalarlm.api_url = "https://llama8btensorwave.cray-lm.com"
-# scalarlm.api_url = "https://vultr.sscalarlm.com"
-# scalarlm.api_url = "https://meta-llama--llama-3-2-3b-instruct.cray-lm.com"
-# scalarlm.api_url = "https://greg1232--cray-cpu-llama-3-2-1b-instruct-fastapi-app.modal.run"
-# scalarlm.api_url = "https://greg1232--cray-nvidia-llama-3-2-3b-instruct-fastapi-app.modal.run"
-
+scalarlm.api_url = "http://localhost:8000"
 
 def get_dataset():
     dataset = []
